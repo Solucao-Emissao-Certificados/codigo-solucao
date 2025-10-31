292a41c1357ba703441af517cc6b00e3d9e527e4
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-10-31 20:21:32 -0300

    .

--- certificados/src/pages/Home/HomeUser/index.jsx
+++ certificados/src/pages/Home/HomeUser/index.jsx
@@ -1,4 +1,4 @@
-import Header from "../../../components/header"
+import Header from "../../../components/Header";
 import HomeCards from "../../../components/HomeCards";
 import Footer from "../../../components/Footer";
 import "./homeuser.css"
