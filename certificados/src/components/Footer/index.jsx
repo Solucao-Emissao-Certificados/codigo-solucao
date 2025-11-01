6c7e993911237c362b54a2caa6c2dd2ec6d1af09
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-11-01 00:01:24 -0300

    .

--- certificados/src/components/Footer/index.jsx
+++ certificados/src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer({className}) {
             <div className="footer-container">
                 <p>&copy; 2025 S.E.C - Todos os direitos reservados.</p>
                 <section className="footer-a">
-                    <a href="https://github.com/lucasdpsa01/codigo-solucao-monitoria.git" target="_blank" rel="">
+                    <a href="https://github.com/Solucao-Emissao-Certificados" target="_blank" rel="">
                     <img src="https://unpwemjugtntyiedmdvc.supabase.co/storage/v1/object/public/images/github.png" alt="página do repositório" height={40}/>
                     </a>
                     <a href="/" class="logout-link"><img src="https://unpwemjugtntyiedmdvc.supabase.co/storage/v1/object/public/images/sair.png" alt="Sair da página" height={40}/></a>
